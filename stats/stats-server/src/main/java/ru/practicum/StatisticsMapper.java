a1fdb40dee2e365cebe5630eb5819a7fb86c319e
olecheBel <ovbelyakova11@yandex.ru>
2024-11-23 22:10:48 +0300

    Дипломный проект этап 1

---
+++ stats/stats-server/src/main/java/ru/practicum/StatisticsMapper.java
@@ -0,0 +1,22 @@
+package ru.practicum;
+
+public class StatisticsMapper {
+    public static StatDto toStatDto(Statistics statistics) {
+        return StatDto.builder()
+                .app(statistics.getApp())
+                .uri(statistics.getUri())
+                .timestamp(statistics.getTimestamp())
+                .build();
+    }
+
+    public static Statistics toStatistics(StatInDto statInDto) {
+        return new Statistics(
+                null,
+                statInDto.getApp(),
+                statInDto.getUri(),
+                statInDto.getIp(),
+                statInDto.getTimestamp()
+        );
+    }
+}
+
---
+++ stats/stats-server/src/main/java/ru/practicum/services/StatisticsService.java
@@ -0,0 +1,16 @@
+package ru.practicum.services;
+
+import ru.practicum.StatDto;
+import ru.practicum.StatInDto;
+import ru.practicum.StatOutDto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface StatisticsService {
+    StatDto create(StatInDto statInDto);
+    List<StatOutDto> get(LocalDateTime start,
+                         LocalDateTime end,
+                         List<String> uris,
+                         Boolean unique);
+}
