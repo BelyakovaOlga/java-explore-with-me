b3c3b53549947b8433efc569feee37785e4cb60a
olecheBel <ovbelyakova11@yandex.ru>
2024-12-07 14:48:21 +0300

    Дипломный проект этап 2 (после проверок GitHub)

--- stats-service/stats-client/src/main/java/ru/practicum/client/BaseClient.java
+++ stats-service/stats-client/src/main/java/ru/practicum/client/BaseClient.java
@@ -15,10 +15,10 @@ public class BaseClient {
         this.restTemplate = restTemplate;
     }
 
-
     protected ResponseEntity<Object> get(String path, @Nullable Map<String, Object> parameters) {
         return makeAndSendRequest(HttpMethod.GET, path, parameters, null);
     }
+
     protected <T> ResponseEntity<Object> post(String path, T body) {
         return makeAndSendRequest(HttpMethod.POST, path, null, body);
     }
